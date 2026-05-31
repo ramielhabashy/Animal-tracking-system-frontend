352c31ac80dab5465ea885e6d3e2541ad4fac225
unknown <eldeene103@gmail.com>
2026-05-31 15:36:47 +0400

    fix API link

---
+++ dist/assets/PrivacyPage-CWIeDVEQ.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-ClFLELTs.js";import{S as t}from"./StaticPage-DzuJCTQG.js";function o(){return r.jsx(t,{slug:"privacy"})}export{o as default};
--- dist/assets/TermsPage-CL04rh85.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-aax6lkOJ.js";import{S as r}from"./StaticPage-CVhc9DQY.js";function a(){return t.jsx(r,{slug:"terms"})}export{a as default};
